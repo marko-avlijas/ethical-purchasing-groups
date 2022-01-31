677f9aa43a6e59668071b827ace77869f5f7055d
Marko Avlijaš <marko.avlijas@gmail.com>
2022-01-31 12:39:35 +0100

    Improve styling of admin users show page

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,18 +1,21 @@
 module AdminHelper
-  def admin_show_field(label, value)
-    value_html = case value
-                 when ActiveSupport::TimeWithZone, DateTime
-                   "#{value.to_s} (#{time_ago_in_words(value)})"
-                 when TrueClass
-                   tag.span "true", class: "badge bg-success"
-                 when FalseClass
-                   tag.span "false", class: "badge bg-danger"
-                 when String, NilClass
-                   value.present? ? value : "&mdash;".html_safe
-                 else
-                   value.to_s
-                 end
+  def admin_value_presenter(value)
+    case value
+    when ActiveSupport::TimeWithZone, DateTime
+      "#{value.to_s} (#{time_ago_in_words(value)})"
+    when TrueClass
+      tag.span "Yes", class: "badge bg-success"
+    when FalseClass
+      tag.span "No", class: "badge bg-danger"
+    when String, NilClass
+      value.present? ? value : "&mdash;".html_safe
+    else
+      value.to_s
+    end
+  end
 
+  def admin_show_field(label, value)
+    value_html = admin_value_presenter(value)
     label_ending = case value
                    when TrueClass, FalseClass
                      "? "
@@ -20,7 +23,8 @@ module AdminHelper
                      ": "
                    end
 
-    tag.li(tag.strong(label + label_ending) + value_html, class: "list-group-item")
+    #tag.li(tag.strong(label + label_ending) + value_html, class: "list-group-item")
+    tag.tr(tag.th(label + label_ending) + tag.td(value_html))
   end
 end
 
